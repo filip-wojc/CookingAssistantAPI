22961276aa0b3a3551f6b2c2b40b861e129ccdee
DarknessoPirate <dp307910@student.polsl.pl>
2024-10-12 19:40:03 +0200

    added ingredient/nutrient list get specific exceptions

---
+++ CookingAssistantAPI/Exceptions/IngredientsNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace CookingAssistantAPI.Exceptions
+{
+    public class IngredientsNotFoundException : Exception
+    {
+        public int Code = 405;
+        public IngredientsNotFoundException(string message) : base(message) {}
+    }
+}
---
+++ CookingAssistantAPI/Exceptions/NutrientsNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace CookingAssistantAPI.Exceptions
+{
+    public class NutrientsNotFoundException : Exception
+    {
+        public int Code = 406;
+        public NutrientsNotFoundException(string message) : base(message) {}
+    }
+}
