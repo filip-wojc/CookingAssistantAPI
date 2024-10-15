89d11f36253aacb6b82c81220e8eb7d60ebd4892
DarknessoPirate <dp307910@student.polsl.pl>
2024-10-15 13:14:42 +0200

    added user email and username validation

--- CookingAssistantAPI/Tools/Validators/UserRegisterValidator.cs
+++ CookingAssistantAPI/Tools/Validators/UserRegisterValidator.cs
@@ -1,18 +1,46 @@
 ﻿using CookingAssistantAPI.DTO.Users;
+using CookingAssistantAPI.Database;
 using FluentValidation;
 
 namespace CookingAssistantAPI.Tools.Validators
 {
     public class UserRegisterValidator : AbstractValidator<UserRegisterDTO>
     {
-        public UserRegisterValidator()
+        private readonly CookingDbContext _context;
+        public UserRegisterValidator(CookingDbContext context)
         {
-            RuleFor(u => u.Email).NotEmpty().EmailAddress();
-            RuleFor(u => u.UserName).NotEmpty().MinimumLength(5).MaximumLength(24);
+            _context = context;
+
+            RuleFor(u => u.Email).NotEmpty().EmailAddress().Must(IsEmailUnique).WithMessage("Email already used");
+            RuleFor(u => u.UserName).NotEmpty().MinimumLength(5).MaximumLength(24).Must(IsUserNameUnique).WithMessage("Username is already taken");
             RuleFor(u => u.Password).NotEmpty()
                 .MinimumLength(5).MaximumLength(30)
                 .Matches(@"[A-Z]").WithMessage("Hasło musi mieć co najmniej jedną wielką literę")
                 .Matches(@"\d").WithMessage("Hasło musi zawierac co najmniej jedną cyfrę");
+        }
+
+        bool IsEmailUnique(string email)
+        { 
+            var user = _context.Users.FirstOrDefault(u => u.Email == email);
+
+            if (user != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        bool IsUserNameUnique(string userName)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == userName);
+
+            if (user != null)
+            {
+                return false;
+            }
+            return true;
+
+
         }
     }
 }
