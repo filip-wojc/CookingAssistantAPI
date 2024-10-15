83725b45b6cdbfad9930f9e13bfdac34e25b8a45
DarknessoPirate <dp307910@student.polsl.pl>
2024-10-15 22:04:24 +0200

    disable https redirection

--- CookingAssistantAPI/Program.cs
+++ CookingAssistantAPI/Program.cs
@@ -66,7 +66,7 @@ namespace CookingAssistantAPI
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseAuthentication();
             app.UseAuthorization();
