5898317361e0e0fb8e8077523a8e69528cdd6410
Filip Wójcik <fw308054@student.polsl.pl>
2024-10-30 11:58:11 +0100

    time in minutes in pdf

--- CookingAssistantAPI/Services/RecipeServices/RecipeService.cs
+++ CookingAssistantAPI/Services/RecipeServices/RecipeService.cs
@@ -161,6 +161,7 @@ namespace CookingAssistantAPI.Services.RecipeServices
             graphics.DrawString($"{recipeDto.AuthorName}", fontBold, XBrushes.Black, new XRect(15, 10, page.Width, page.Height), XStringFormats.TopLeft);
             graphics.DrawString($"Difficulty: {recipeDto.Difficulty}", fontBold, XBrushes.Black, new XRect(-15, 10, page.Width, page.Height), XStringFormats.TopRight);
             graphics.DrawString($"Serves: {recipeDto.Serves}", fontBold, XBrushes.Black, new XRect(-15, 25, page.Width, page.Height), XStringFormats.TopRight);
+            graphics.DrawString($"Time to prepare: {recipeDto.TimeInMinutes}", fontBold, XBrushes.Black, new XRect(-15, 40, page.Width, page.Height), XStringFormats.TopRight);
             var splittedDesc = SplitIntoEqualParts(recipeDto.Description, 45);
             var lineHeight = 35;
             foreach (var tx in splittedDesc)
